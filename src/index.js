daf72c838136d2cde766a23ad2f66fc2a0f0abfd
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-10-25 10:56:12 +0200

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+    const strToArr = str.split('');
+    const arrLen = strToArr.length;
+    const confLen = bracketsConfig.length;
+    for (let count = 0; count < arrLen; count += 1) {
+        for (let counter = 0; counter < confLen; counter += 1) {
+            if (strToArr[count] === bracketsConfig[counter][0] && strToArr[count + 1] === bracketsConfig[counter][1]) {
+                strToArr.splice(count, 2);
+                count = -1;
+            }
+        }
+    }
+
+    return !strToArr.length;
 }
+
+
+module.exports = check;
